1ab8fa6131b515e43efb92c5673d04f80c408620
Guolong Wang <13121593633@163.com>
2020-01-10 10:15:56 +0800

    synchronized volatile 可见性

---
+++ src/main/java/com/wangguolong/concurrency/example/count/CountExample4.java
@@ -0,0 +1,60 @@
+package com.wangguolong.concurrency.example.count;
+
+import com.wangguolong.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/1/9 10:53 上午
+ */
+@Slf4j
+@NotThreadSafe
+public class CountExample4 {
+    // 请求总数
+    public static int clientTotal = 5000;
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+    public static volatile int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        // 首先定义一个线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        // 定义信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        // 定义计数器
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        // 把请求全部放入线程池中
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    // 判断当前线程能否被执行
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception : ", e);
+                }
+                // 没执行完一个之后调用一次countDown, 计数值就会减一个
+                countDownLatch.countDown();
+            });
+        }
+        // 保证计数器为0
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count : {}", count);
+    }
+
+    private static void add() {
+        count++;
+        // 1. 取出主内存的count值
+        // 2. +1
+        // 3. 重新写回主内存
+        // 两个线程同时加一后一起写回主内存，这就丢掉了一次
+    }
+}
