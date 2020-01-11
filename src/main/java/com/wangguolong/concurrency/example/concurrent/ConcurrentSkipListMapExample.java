5510dc3e86482d0c07d9d1cefbc678b4572b8d1b
Guolong Wang <13121593633@163.com>
2020-01-11 10:04:23 +0800

    并发容器

---
+++ src/main/java/com/wangguolong/concurrency/example/concurrent/ConcurrentSkipListMapExample.java
@@ -0,0 +1,56 @@
+package com.wangguolong.concurrency.example.concurrent;
+
+import com.wangguolong.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * @author Wang Guolong
+ * @version 1.0
+ * @date 2020/1/9 10:53 上午
+ */
+@Slf4j
+@ThreadSafe
+public class ConcurrentSkipListMapExample {
+    // 请求总数
+    public static int clientTotal = 5000;
+    // 同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    private static Map<Integer, Integer> map = new ConcurrentSkipListMap<>();
+
+    public static void main(String[] args) throws Exception {
+        // 首先定义一个线程池
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        // 定义信号量
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        // 定义计数器
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        // 把请求全部放入线程池中
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    // 判断当前线程能否被执行
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception : ", e);
+                }
+                // 没执行完一个之后调用一次countDown, 计数值就会减一个
+                countDownLatch.countDown();
+            });
+        }
+        // 保证计数器为0
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size:{}", map.size());
+    }
+
+    private static void update(int i) {
+        map.put(i, i);
+    }
+}
